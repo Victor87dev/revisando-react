3928d71b7b4c2a837e40551819825c84ae33831b
Victor87dev <devsjoaovvictor@gmail.com>
2024-01-31 13:22:14 -0300

    Curso react: React Icons

--- revisando-react/src/components/layout/Footer.jsx
+++ revisando-react/src/components/layout/Footer.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Styles from "./Footer.module.css";
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 
 const Footer = ()=>{
 
    return (
-    <footer>Rodápe</footer>
+    <footer>
+        <ul className={Styles.list_estilo}>
+            <li><FaFacebook /></li>
+            <li><FaInstagram /></li>
+            <li><FaLinkedin /></li>
+        </ul>
+        <p>nosso rodápe</p>
+    </footer>
    )
 
 }
